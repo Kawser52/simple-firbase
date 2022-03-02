dffed9483faa29287a81b8374ffe2768612783eb
TechDev-CEO <shoriful521399@gmail.com>
2022-03-02 19:24:34 +0600

    Firebase Authentication Ccomplete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import {getAuth, GoogleAuthProvider, signInWithPopup, GithubAuthProvider, signOut} from 'firebase/auth';
 import './App.css';
 import {useState} from 'react';
 import initializeAuthentication from './Firebase/firebase.initialize';
 
 initializeAuthentication();
-const provider=new GoogleAuthProvider();
+const gooleProvider=new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
 
-
+const auth=getAuth();
 function App() {
   const [user, setUser] = useState([]);
   const handleGoogleSignIn = ()=>{
-    const auth=getAuth();
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, gooleProvider)
     .then(result=>{
       const {email, displayName,photoURL} = result.user;
       const loggedUser = {
@@ -22,13 +22,40 @@ function App() {
       setUser(loggedUser);
     })
   }
+  const handleGithubSignIn=()=>{
+      signInWithPopup(auth, githubProvider)
+      .then(result=>{
+        const {displayName, photoURL} = result.user;
+        const loggedUser = {
+          name:displayName,
+          photo: photoURL
+        }
+        setUser(loggedUser)
+      })
+  }
+  const handleSignOut=()=>{
+    signOut(auth)
+    .then(()=>{
+      setUser({});
+    })
+  }
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      {!user.name?
+      <div>
+        <button onClick={handleGoogleSignIn}>Google Sign In</button>
+        <button onClick={handleGithubSignIn}>Github Sign In</button>
+        </div>:
+      <button onClick={handleSignOut}>Log Out</button>
+        
+      }
+      
       <br/>
       {
-        user.email && <div>
+        user.name && <div>
               <h2>Welcome: {user.name}</h2>
+              <h4>Kita obosta: {user.email}</h4>
+              <img src={user.photo} alt="" />
           </div>
 
       }
